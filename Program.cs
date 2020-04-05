0bd589c045792a584227a66113e49cee4074b637
Азиз Исматов <yourGitHub@example.co>
2020-04-05 18:28:17 +0500

    Создали класс Converter и конструктор

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Converter
+    {
+     public string usd{set;get;}
+     public string eur{set;get;}
+     public string rub{set;get;}
+     public Converter(string usd, string eur, string rub)
+     {
+         
+     }
+
+    }
+}
